66ce8041d567a13955438cdc75f6910ccc0f6c0d
David Zuber <zuber.david@gmx.de>
2015-04-14 21:20:38 +0200

    Fix url and add git identity

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,8 @@ echo "Deploying"
 pip install pip2pi
 git clone https://github.com/storax/travis-pyside-test.git
 cd travis-pyside-test
+git config user.name "David Zuber"
+git config user.email "zuber.david@gmx.de"
 git checkout gh-pages
 cd ..
 cp $WHEELHOUSE/*.whl travis-pyside-test
@@ -11,6 +13,5 @@ dir2pi --no-symlink --normalize-package-names travis-pyside-test
 cd travis-pyside-test
 git add -A
 git commit -m"Add PySide build $TRAVIS_BUILD_NUMBER to index."
-url="https://$GITHUBUSERNAME:$GITHUBPASSWORD@github.com/stoax/travis-pyside-test.git"
-git remote set-url origin $url
+git remote set-url origin "https://${GITHUBUSERNAME}:${GITHUBPASSWORD}@github.com/stoax/travis-pyside-test.git"
 git push origin gh-pages
