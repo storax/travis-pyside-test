3e1d81027dd5b8b57f6f2ede43fcee7fb251b8f7
David Zuber <zuber.david@gmx.de>
2015-04-14 13:35:49 +0200

    Put Xvfb in background

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ pip install -v -f $WHEELHOUSE --no-index pyside
 # Post install needed for pyside wheel
 python $WHEELHOUSE/pyside_postinstall.py -install
 # Create virtual frame buffer because we run on headless machine
-Xvfb :0 -screen 0 1600x1200x24
+Xvfb :0 -screen 0 1600x1200x24 &
 export DISPLAY=:0.0
 
 #test
