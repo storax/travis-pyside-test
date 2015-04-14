9baa5f46c43ba4be46ea6e7a9f91f91b25c93a60
David Zuber <zuber.david@gmx.de>
2015-04-14 23:03:15 +0200

    New token, no credentials file

--- deploy.sh
+++ deploy.sh
@@ -7,12 +7,9 @@ git clone https://github.com/storax/travis-pyside-test.git deploy
 cd deploy
 git config user.name "David Zuber"
 git config user.email "zuber.david@gmx.de"
-git config credential.helper "store --file=.git/credentials"
-> .git/credentials <<< "https://${GITHUBTOKEN}:@github.com"
 git checkout gh-pages
 cp $WHEELHOUSE/*.whl .
 dir2pi --no-symlink --normalize-package-names .
 git add -A
 git commit -m"Add PySide build ${TRAVIS_BUILD_NUMBER} to index."
-git push origin gh-pages
-rm -f .git/credentials
+git push "https://${GITHUBTOKEN}:@github.com/storax/travis-pyside-test.git" gh-pages
